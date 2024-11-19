c3d0c0bb8af51197f438bb8301befbf9d22804fb
Niko <radosz2003@gazeta.pl>
2024-11-19 17:30:39 +0100

    loading products from csv

--- lib/machineOps.go
+++ lib/machineOps.go
@@ -5,15 +5,6 @@ import (
 	"fmt"
 )
 
-func CreateProductsTable(db *sql.DB) (sql.Result, error){
-	sql := `CREATE TABLE IF NOT EXISTS products (
-		id INTEGER PRIMARY KEY,
-		name TEXT UNIQE NOT NULL,
-		production_time REAL
-	);`
-	return db.Exec(sql)
-}
-
 func CreateMachinesTable(db *sql.DB) (sql.Result, error){
 	sql := `CREATE TABLE IF NOT EXISTS machines (
 		id INTEGER PRIMARY KEY,
@@ -28,35 +19,6 @@ func CreateMachinesTable(db *sql.DB) (sql.Result, error){
 		energy_consumption REAL
 	);`
 	return db.Exec(sql)
-}
-// parent = product produced
-//child = product requred
-func CreateBomTable(db *sql.DB) (sql.Result, error){
-	sql := `CREATE TABLE IF NOT EXISTS BOM (
-		parent_id INTEGER NOT NULL,
-		parent_quantity INTEGER,
-		child_id INTEGER,
-		child_quantity INTEGER,
-		byproduct_id INTEGER,
-		byproduct_quantity INTEGER
-		);`
-	return db.Exec(sql)
-}
-
-func InsertProduct(db *sql.DB, Product *Product) (int64, error){
-	sql :=`INSERT INTO products (
-		name,
-		production_time)
-		VALUES (?,?);`
-
-	result, err :=db.Exec(sql,
-		Product.Name,
-		Product.Production_time)
-
-	if err !=nil{
-		return 0,err
-	}
-	return result.LastInsertId()
 }
 
 func InsertMachine(db *sql.DB, Machine *Machine) (int64, error){
@@ -89,25 +51,6 @@ func InsertMachine(db *sql.DB, Machine *Machine) (int64, error){
 	return result.LastInsertId()
 }
 
-func DeleteProduct(db *sql.DB, arg interface{}) (int64, error){
-	var(
-		result sql.Result
-		err error
-	)
-	switch i := arg.(type){
-		case int:
-			sql :=`DELETE FROM products WHERE id=?`
-			result, err=db.Exec(sql,i)
-		case string:
-			sql :=`DELETE FROM products WHERE name=?`
-			result, err=db.Exec(sql,i)
-		default:
-			return 0, fmt.Errorf("wrong argument type: supported types int-id string-name")
-	}
-	if err !=nil{return 0,err}
-	return result.RowsAffected()
-}
-
 func DeleteMachine(db *sql.DB, arg interface{}) (int64, error){
 	var (
 		result sql.Result
@@ -125,4 +68,34 @@ func DeleteMachine(db *sql.DB, arg interface{}) (int64, error){
 	}
 	if err != nil {return 0,err}
 	return result.RowsAffected()
+}
+
+func ReadCSV(filename string) ([]Country, error) {
+    // Open the CSV file
+    file, err := os.Open(filename)
+    if err != nil {
+        return nil, err}
+    defer file.Close()
+
+    reader := csv.NewReader(file)
+    records, err := reader.ReadAll()
+    if err != nil {return nil, err}
+
+    var countries []Country
+    for _, record := range records[1:] { // Skip header row
+        population, err := strconv.Atoi(record[1])
+        if err != nil {return nil, err}
+
+        area, err := strconv.Atoi(record[2])
+        if err != nil {return nil, err}
+        
+				country := Country{
+            Name:       record[0],
+            Population: population,
+            Area:       area,
+        }
+        countries = append(countries, country)
+    }
+
+    return countries, nil
 }
